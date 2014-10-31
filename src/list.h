b98202f19fa5c94b5afc6560ae6fe83a3f96ae93
WaitDodot <chen1990214@qq.com>
2014-10-31 06:43:12 -0700

    list

--- src/list.h
+++ src/list.h
@@ -19,9 +19,42 @@ class list
 		typedef T&			reference;
 		typedef const T&	const_reference;
 		typedef std::size_t size_type;
+		
+		static const size_type LIST_HEAD  = 0;
+		static const size_type LIST_TAIL  = 1;
 
+		class listNode{
+			public:
+				value_type data;
+				listNode* previous;
+				listNode* next;
+		};
+
+		list(){
+			head = tail = 0;
+			m_size = 0;
+		}
+		
+		void insert( reference value , pos = LIST_TAIL){
+			__insert(value,pos);
+		}
 	pravite:
-		T data;
+		//function.
+		void __insert(reference value , size_type pos){
+			if ( !head || !tail )
+			{	
+				listNode
+			}
+			if ( pos == LIST_TAIL )
+			{
+				
+			}
+		}
+		
+		// data.
+		listNode* head;
+		listNode* tail;
+		size_type m_size;
 
 };
 
